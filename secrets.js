b3c653b4f8934df41ba9f1dba4b85118f16282b7
Shivam Patel <developer.sp4799@gmail.com>
2021-08-21 21:10:31 +0530

    udpate .gitignore

--- secrets.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  playlist_url:"PLFG0ZUIGxfMV2mxPB7boBBxLyeOENVTzo",
-  user_id:"31rkypzldnte3775uafxs7pjl32u",
-  yt_api:"AIzaSyCvZIFHdHFvY2WvWDT0D3Kt6jVMPx0HK7A",
-  spotify_token: "BQAivYoTMbZTl5L7dEZbrls-A0SCXmZWKASvOW6kUyLdF7XfHzi96l5D7tAilNt_zDo7Mm22aKXHBxrMplCbDD7KPB_mDABfuFLJIvxdHtd0Vn3KhhLX34Bsx_-VbVpjVNNXIsPXFEnr-O4fSZ9ah_sqZNqtlG8ZPIi4D-wQNQwcfc9ou-wGVaMlgcARaZi_lITQMLgaRTmM9Fl3F2alg65KUsv_Ss8Fo1uyMvvgYgwynNsL4qDho4q7s4BXRNWX0T0BLZqKXN-ANCMQRABBG4nUAJhd0_qEcrKBekrhGA"
-}; 
