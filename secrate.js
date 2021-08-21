83dade7be9b296f73bedf077939e8327de451e66
Shivam Patel <developer.sp4799@gmail.com>
2021-08-21 19:23:22 +0530

    add secrate_sample.js

--- secrate.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  client_id: "be0c3b747a4541aba0e8d5df7597112d",
-  client_secret: "d623d3267d724f51b8a62e4c90ca6a6b" // Your secret
-}; // Your client id
---
+++ secrate_sample.js
@@ -0,0 +1,4 @@
+module.exports = {
+  client_id: "", //// Your  spotify client id
+  client_secret: "" // Your spotify developer secret
+}; 
